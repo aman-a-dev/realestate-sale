c28273c35ca5c4a8f13d735e22e0afbd85471e70
aman-a-dev <amanuelantenha@gmail.com>
2026-08-14 11:20:18 +0300

    mob real estate

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 // brand
 export const BRAND = {
-  name: "Joburg Real estate",
+  name: "Mon Real estate",
   tagline: "Where Vision Meets Address",
   founded: 2015,
   location: "Ethiopia Addis Ababa",
