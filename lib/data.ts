1a541383ff12fc78b2bfd5258ba0ee39737225ca
aman-a-dev <amanuelantenha@gmail.com>
2026-08-15 14:25:12 +0300

    melate elemental

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 // brand
 export const BRAND = {
-  name: "Nile Properties",
+  name: "Melat Elemental Properties",
   tagline: "Where Vision Meets Address",
   founded: 2015,
   location: "Ethiopia Addis Ababa",
